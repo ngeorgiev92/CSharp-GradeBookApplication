c901665efe395649dd47c55656632ba9e16cc0b1
Ned <ngeorgiev92@gmail.com>
2021-05-20 15:11:11 +0100

    OverrideRankedGradebooksGetLetterGradeMethod
    Overrode RankedGradebook's GetLetterGradeTests method and added logic to return letter grades based on banded percentages.

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -8,7 +8,23 @@ namespace GradeBook.GradeBooks
     {
         public RankedGradeBook(string name) : base(name)
         {
-            this.Type = Enums.GradeBookType.Ranked;
+            this.Type = Enums.GradeBookType.Ranked;            
+        }
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work.");
+            }
+            if (averageGrade >= 80)
+                return 'A';
+            if (averageGrade >= 60 && averageGrade < 80)
+                return 'B';
+            if (averageGrade >= 40 && averageGrade < 60)
+                return 'C';
+            if (averageGrade >= 20 && averageGrade < 40)
+                return 'D';
+            return 'F';
         }
     }
 }
